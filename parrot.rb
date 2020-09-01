3ba68fe010a93d36e5c4abb872a80092d48c5558
Kechi Boniface <bonifacekechi@gmail.com>
2020-09-01 00:27:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(defaultphrase = "Squawk!", )
+def parrot(defaultphrase = "Squawk!", phrase = "Pretty bird!")
 puts "#{defaultphrase}"
+puts "#{phrase}"
 
 end
